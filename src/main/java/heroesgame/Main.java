99f6f21bbd1e83119a2575e8feb81bbd2070c7f8
nastiazaya <nastiazaytzev@gmail.com>
2021-04-24 16:58:56 +0300

    Add ew main to hero game

--- src/main/java/heroesgame/Main.java
+++ src/main/java/heroesgame/Main.java
@@ -3,12 +3,12 @@ package heroesgame;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-       GameManager gameManager = new GameManager();
-       /* Hero hero1 = HeroFactory.createHero();
+        GameManager gameManager = new GameManager();
+        Hero hero1 = HeroFactory.createHero();
         Hero hero2 = HeroFactory.createHero();
-        gameManager.fight(hero1, hero2);*/
+        gameManager.fight(hero1, hero2);
 
-        // with Supplier
+        // hero factory with Supplier
         HeroFactorySupplier heroFactorySupplier = new HeroFactorySupplier();
         Hero heroOne = heroFactorySupplier.createHero();
         Hero heroTwo = heroFactorySupplier.createHero();
