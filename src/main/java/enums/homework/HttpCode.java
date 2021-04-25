5ff6fb2f4f146b1b01448dfa5f99d4dfa0b51787
nastiazaya <nastiazaytzev@gmail.com>
2021-04-26 00:35:22 +0300

    Add find code method to httpCode enum

--- src/main/java/enums/homework/HttpCode.java
+++ src/main/java/enums/homework/HttpCode.java
@@ -3,19 +3,20 @@ package enums.homework;
 import java.util.Arrays;
 
 public enum HttpCode {
-    INFORMATIONAL(100, 199),
-    SUCCESS(200, 299),
-    REDIRECTION(300,399),
-    CLIENT_ERROR(400, 499),
-    SERVER_ERROR(500, 599);
+    INFORMATIONAL(100, 199,new InformationCodeHttpHandler()),
+    SUCCESS(200, 299, new SuccessCodeHttpHandler()),
+    REDIRECTION(300,399, new RedirectionCodeHttpHandler()),
+    CLIENT_ERROR(400, 499, new ClientErrorCodeHttpHandler()),
+    SERVER_ERROR(500, 599, new ServerErrorCodeHttpHandler());
 
     private final int min;
     private final int max;
+    private final HttpHandler httpHandler;
 
-
-    HttpCode(int min, int max) {
+    HttpCode(int min, int max, HttpHandler httpHandler) {
         this.min = min;
         this.max = max;
+        this.httpHandler = httpHandler;
     }
 
     public static HttpCode getHttpCode(int code) {
@@ -23,5 +24,10 @@ public enum HttpCode {
         return Arrays.stream(httpCode).filter(item -> code >= item.min && code <= item.max)
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException(code + " not supported"));
+    }
+
+
+    public static HttpHandler findCode(int code) {
+        return getHttpCode(code).httpHandler;
     }
 }
--- src/main/java/enums/homework/HttpCodeHandler.java
+++ src/main/java/enums/homework/HttpCodeHandler.java
@@ -5,17 +5,8 @@ import java.util.Map;
 
 public class HttpCodeHandler {
 
-    static Map<HttpCode, HttpHandler> httpCodeHandlers  = new HashMap<HttpCode, HttpHandler>() {{
-        put(HttpCode.INFORMATIONAL, new InformationCodeHttpHandler());
-        put(HttpCode.SUCCESS, new SuccessCodeHttpHandler());
-        put(HttpCode.REDIRECTION, new RedirectionCodeHttpHandler());
-        put(HttpCode.CLIENT_ERROR, new ClientErrorCodeHttpHandler());
-        put(HttpCode.SERVER_ERROR, new ServerErrorCodeHttpHandler());
-    }};
-
-
-
     public void handleHttpCode(int httpCode){
-        httpCodeHandlers.get(HttpCode.getHttpCode(httpCode)).handle();
+
+        HttpCode.findCode(httpCode).handle();
     }
 }
