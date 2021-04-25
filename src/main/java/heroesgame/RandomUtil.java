0e925c89e3eaf9ab3fee73b079912e74cf99b894
nastiazaya <nastiazaytzev@gmail.com>
2021-04-25 13:43:12 +0300

    Change random hero

--- src/main/java/heroesgame/RandomUtil.java
+++ src/main/java/heroesgame/RandomUtil.java
@@ -1,5 +1,6 @@
 package heroesgame;
 
+import java.util.List;
 import java.util.Random;
 
 public class RandomUtil {
@@ -7,5 +8,9 @@ public class RandomUtil {
 
     public static int getNumberBetween(int min, int max){
         return random.nextInt((max - min) + 1) + min;
+    }
+
+    public static <T> T getRandomItem(List<T> list) {
+        return list.get(getNumberBetween(0,list.size()-1));
     }
 }
