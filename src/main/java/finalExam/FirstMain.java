f61adf04864b5502d4a530b0b0f3013d1d6f909b
nastiazaya <nastiazaytzev@gmail.com>
2021-05-02 12:44:02 +0300

    Add quotes to the list

--- src/main/java/finalExam/FirstMain.java
+++ src/main/java/finalExam/FirstMain.java
@@ -5,7 +5,6 @@ public class FirstMain {
         QuoteProducer quoteProducer = new QuoteProducer();
         for (int i = 0; i < 2; i++) {
             quoteProducer.start();
-            System.out.println(i);
         }
     }
 }
--- src/main/java/finalExam/QuoteProducer.java
+++ src/main/java/finalExam/QuoteProducer.java
@@ -12,9 +12,10 @@ import java.util.Random;
 
 @NoArgsConstructor
 public class QuoteProducer {
-    private  final List<String> quotes = List.of("bla bla",
-            "hvdkjxh",
-            "shflidhl");
+    private  final List<String> quotes = List.of("Whether you think you can or you think you can't, you're right",
+            "Life is what happens when you're busy making other plans.",
+            "The way to get started is to quit talking and begin doing.",
+            "The future belongs to those who believe in the beauty of their dreams.");
 
     private int countID = 1;
     private Random random = new Random();
